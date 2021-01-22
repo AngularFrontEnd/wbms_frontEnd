d3a4c4b718a4ded1993af40e62bfc1980f906b20
Uaena96 <antonette.biore@student.passerellesnumeriques.org>
2021-01-22 21:40:22 +0800

    Added Files

---
+++ src/app/meter-reading/meter-reading.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-meter-reading',
+  templateUrl: './meter-reading.component.html',
+  styleUrls: ['./meter-reading.component.scss']
+})
+export class MeterReadingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  houseId;
+  meterReading;
+
+
+  getMeterReadingData(data: any) {
+    console.log(data);
+
+  }
+}
