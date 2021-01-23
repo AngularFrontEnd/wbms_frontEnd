188710d01a7d491e3377a1d2acde5e7b4f9583be
john-paul.vistal <johnpaulvistal@gmail.com>
2021-01-23 13:29:21 +0800

    Added Changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,9 @@ import { TransactionComponent } from './transaction/transaction.component';
 import { CustomerComponent } from './customer/customer.component';
 import { EmployeeRecordsComponent } from './employee-records/employee-records.component';
 import { SettingsComponent } from './settings/settings.component';
+import { RegisterHouseholdComponent } from './register-household/register-household.component';
+import { MeterReadingComponent } from './meter-reading/meter-reading.component';
+import { RegisterEmployeeComponent } from './register-employee/register-employee.component';
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
@@ -16,7 +19,11 @@ const routes: Routes = [
   { path: 'transaction', component: TransactionComponent },
   { path: 'customer', component: CustomerComponent },
   { path: 'employee-records', component: EmployeeRecordsComponent },
-  { path: 'settings', component: SettingsComponent }
+  { path: 'settings', component: SettingsComponent },
+  { path: 'register-new-household', component: RegisterHouseholdComponent },
+  { path: 'meter-reading', component: MeterReadingComponent },
+  { path: 'register-employee', component: RegisterEmployeeComponent }
+
 ];
 
 @NgModule({
