b36914553873f06f30c532cb5f0c0a4ac10a1000
2ndyrGroupA <2ndyrGroupA@pn.local>
2021-01-26 20:27:03 +0800

    test

--- server.js
+++ server.js
@@ -1,10 +1,7 @@
 const express = require('express')
-
-const path = require('path')
-
 const app = express()
 
-app.use(express.static('./dist/wbms-project/'))
+app.use(express.static('./dist/wbms-project'))
 
 app.get('/*', req, res =>{
         res.sendFile('index.html', 
