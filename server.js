d0ffc89e5c3b2ea13e6568af7b96bd6e865fb799
2ndyrGroupA <2ndyrGroupA@pn.local>
2021-01-26 21:27:43 +0800

    update port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express()
 app.use(express.static('./dist/wmbs-project'))
 
 app.get('/*', (req, res) =>{
-        res.sendFile('index.html',  { root: 'dist/wmbs-project/'})
+    res.sendFile('index.html',  { root: 'dist/wmbs-project/'})
 })
 
-app.listen(process.env.port || 8080)
+app.listen(process.env.PORT || 8080)
